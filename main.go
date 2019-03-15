283b60f8b5f5cf40ad3734b4d63521aed7dc6dda
Ivo Ribeiro <ivolopesribeiro15@gmail.co>
2019-03-15 00:56:59 +0000

    Private key, command line flags, multiaddress

--- main.go
+++ main.go
@@ -2,24 +2,52 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"flag"
 	"fmt"
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-crypto"
+	"github.com/multiformats/go-multiaddr"
+	"os"
 )
 
 func main() {
+	help := flag.Bool("help", false, "Display Help")
+	cfg := parseFlags()
 
-	// The context governs the lifetime of the libp2p node
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	if *help {
+		fmt.Printf("Usage: \n   Run './sbottle'\nor Run './sbottle -port [port]'\n")
+		os.Exit(0)
+	}
 
-	// To construct a simple host with all the default settings, just use `New`
-	h, err := libp2p.New(ctx,
-		// Set your own listen address
-		// The config takes an array of addresses, specify as many as you want.
-		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/9000"), )
+	fmt.Printf("[*] Listening on: %s with port: %d\n", cfg.listenHost, cfg.listenPort)
+
+	ctx := context.Background()
+	r := rand.Reader
+
+	// Creates a new RSA key pair for this host.
+	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Hello World, my hosts ID is %s\n", h.ID())
+	// 0.0.0.0 will listen on any interface device.
+	sourceMultiAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%d", cfg.listenHost, cfg.listenPort))
+
+	// libp2p.New constructs a new libp2p Host.
+	// Other options can be added here.
+	host, err := libp2p.New(
+		ctx,
+		libp2p.ListenAddrs(sourceMultiAddr),
+		libp2p.Identity(prvKey),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Host ID %s\n", host.ID())
+	fmt.Printf("\n[*] Multiaddress: /ip4/%s/tcp/%v/p2p/%s\n", cfg.listenHost, cfg.listenPort, host.ID().Pretty())
+
+	select {} //wait here
 }
