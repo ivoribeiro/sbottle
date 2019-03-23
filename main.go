7d69b9f5797ad9f185af826422b4f656f7e48ab7
Ivo Ribeiro <ivolopesribeiro15@gmail.co>
2019-03-23 19:00:52 +0000

    stream handle

--- main.go
+++ main.go
@@ -1,15 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"crypto/rand"
 	"flag"
 	"fmt"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-crypto"
+	inet "github.com/libp2p/go-libp2p-net"
+	"github.com/libp2p/go-libp2p-protocol"
 	"github.com/multiformats/go-multiaddr"
 	"os"
 )
+
+func handleStream(stream inet.Stream) {
+	fmt.Println("Got a new stream!")
+
+	// Create a buffer stream for non blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+	go readData(rw)
+	go writeData(rw)
+
+	// 'stream' will stay open until you close it (or the other side closes it).
+}
+
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, err := rw.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from buffer")
+			panic(err)
+		}
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from stdin")
+			panic(err)
+		}
+
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		if err != nil {
+			fmt.Println("Error writing to buffer")
+			panic(err)
+		}
+		err = rw.Flush()
+		if err != nil {
+			fmt.Println("Error flushing buffer")
+			panic(err)
+		}
+	}
+}
 
 func main() {
 	help := flag.Bool("help", false, "Display Help")
@@ -46,6 +105,10 @@ func main() {
 		panic(err)
 	}
 
+	// Set a function as stream handler.
+	// This function is called when a peer initiates a connection and starts a stream with this peer.
+	host.SetStreamHandler(protocol.ID(cfg.ProtocolID), handleStream)
+
 	fmt.Printf("Host ID %s\n", host.ID())
 	fmt.Printf("\n[*] Multiaddress: /ip4/%s/tcp/%v/p2p/%s\n", cfg.listenHost, cfg.listenPort, host.ID().Pretty())
 
@@ -56,6 +119,19 @@ func main() {
 
 	if err := host.Connect(ctx, peer); err != nil {
 		fmt.Println("Connection failed:", err)
+	}
+
+	// open a stream, this stream will be handled by handleStream other end
+	stream, err := host.NewStream(ctx, peer.ID, protocol.ID(cfg.ProtocolID))
+
+	if err != nil {
+		fmt.Println("Stream open failed", err)
+	} else {
+		rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+		go writeData(rw)
+		go readData(rw)
+		fmt.Println("Connected to:", peer)
 	}
 
 	select {} //wait here
